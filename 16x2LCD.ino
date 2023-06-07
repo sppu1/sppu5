76e77fedf32c12e2aa33c15026121c8f01cd1a4e
sppu1 <115922004+sppu1@users.noreply.github.com>
2023-06-07 23:23:44 +0530

    Update 16x2LCD.ino

--- 16x2LCD.ino
+++ 16x2LCD.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(6,7,2,3,4,5);
+LiquidCrystal lcd(6,7,2,3,4,5); // Creates an LCD object. Parameters: (RegisterSelect, enable, d4, d5, d6, d7)
 
 
 void setup() 
